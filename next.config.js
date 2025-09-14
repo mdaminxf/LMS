0df164244903ef1da6acb2d27f660f4f1a40872b
mohammadamin <mdaminjilani313@gmail.com>
2025-09-14 12:12:14 +0530

    Your message

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // REMOVE this line
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
